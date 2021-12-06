a5bd234154aee8aa618feb7adfb3db063e4c0e02
Chris Clifton <git@chrisclifton.me>
2021-12-06 18:00:22 +0000

    Add browser sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .browserSync({
+        host: '127.0.0.1',
+        proxy: process.env.APP_URL,
+        open: false
+    });
