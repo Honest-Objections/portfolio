d12431d6810be3305089adc24c16bdc6e0c70d76
Chris Clifton <git@chrisclifton.me>
2021-12-06 18:03:59 +0000

    Add tailwind css and setup webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,26 @@
 const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+const tailwindcss = require('tailwindcss'); /* Add this line at the top */
 
 mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        postCss: [tailwindcss('./tailwind.config.js')],
+    })
     .browserSync({
         host: '127.0.0.1',
         proxy: process.env.APP_URL,
         open: false
     });
+
+// mix.webpackConfig({
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.worker.js$/,
+//                 loader: 'worker-loader',
+//                 options: { /* ... */ }
+//             },
+//         ]
+//     }
+// }); 
