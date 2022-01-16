e8dacb233e118f581e4704067028555611e50b91
Chris Clifton <git@chrisclifton.me>
2022-01-16 02:30:06 +0000

    Show name of newly uploaded images

--- resources/js/components/ImageUploader.vue
+++ resources/js/components/ImageUploader.vue
@@ -1,10 +1,14 @@
 <template>
     <div v-for="image in images" :key="image.index" class="inline relative">
-        <input :ref="`img-${image.index}`" type="file" :name="`${inputName}[${image.index}]`" :class="!image.src ? 'inline' : 'hidden'" @change="imageUploaded(image.index)" accept="image/*" />
-        <img :class="image.src ? 'inline' : 'hidden'" :src="image.src" />
-        <div class="absolute bottom-0" v-if="image.id !== undefined">
-            <label>{{ image.originalName }}</label>
-        </div>
+        <label class="block">
+            <img :class="image.src ? 'inline' : 'hidden'" :src="image.src" />
+            <div class="absolute bottom-0" v-if="image.originalName">
+                <label>{{ image.originalName }}</label>
+            </div>
+            <div v-if="image.id === undefined">
+                <input :ref="`img-${image.index}`" type="file" :name="`${inputName}[${image.index}]`" :class="!image.src ? 'inline' : 'hidden'" @change="imageUploaded(image.index)" accept="image/*,video/*" class="block w-full text-sm" />
+            </div>
+        </label>
     </div>
 </template>
 
@@ -15,6 +19,7 @@ export default {
         return {
             images: [{
                 index: 0,
+                originalName: '',
                 src: null
             }]
         }
@@ -30,6 +35,7 @@ export default {
         'imageUploaded': function (imgIndex) {
             var file = this.$refs[`img-${imgIndex}`][0].files
             if (file) {
+                this.images[imgIndex].originalName = file[0].name
                 this.images[imgIndex].src = URL.createObjectURL(file[0])
                 this.images.push({ index: this.images.length, src: null })
             }
