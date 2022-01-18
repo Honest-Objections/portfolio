a121d056f1f2c043379f5c72f57791ee6f01b071
Chris Clifton <git@chrisclifton.me>
2022-01-18 13:59:15 +0000

    Upatable background images

--- resources/js/components/ImageUploader.vue
+++ resources/js/components/ImageUploader.vue
@@ -2,21 +2,27 @@
     <div v-for="image in images" :key="image.index" class="inline relative">
         <label class="block">
             <img :class="image.src ? 'inline' : 'hidden'" :src="image.src" />
-            <div class="absolute bottom-0" v-if="image.originalName">
+            <div class="absolute bottom-0 w-full" v-if="image.originalName">
                 <label>{{ image.originalName }}</label>
+                <label class="float-right" v-if="image.originalName.match(/.(jpg|jpeg|png)$/i)">
+                    Background:
+                    <input ref="background" type="checkbox" @click="selectedAsBackground(image.id)" :checked="background == image.id">
+                </label>
             </div>
             <div v-if="image.id === undefined">
-                <input :ref="`img-${image.index}`" type="file" :name="`${inputName}[${image.index}]`" :class="!image.src ? 'inline' : 'hidden'" @change="imageUploaded(image.index)" accept="image/*,video/*" class="block w-full text-sm" />
+                <input type="file" :name="`${inputName}[${image.index}]`" :class="!image.src ? 'inline' : 'hidden'" @change="imageUploaded(image.index)" accept="image/*,video/*" class="block w-full text-sm" />
             </div>
         </label>
     </div>
+    <input name="background" type="hidden" :value="background" />
 </template>
 
 <script>
 export default {
-    props: ['inputName', 'existing'],
+    props: ['inputName', 'existing', 'currentBg'],
     data() {
         return {
+            background: null,
             images: [{
                 index: 0,
                 originalName: '',
@@ -29,7 +35,7 @@ export default {
         JSON.parse(this.existing).forEach(image => {
             self.images.push(image); 
         });
-        console.log(this.images) 
+        self.background = self.currentBg; 
     },
     methods: {
         'imageUploaded': function (imgIndex) {
@@ -39,6 +45,9 @@ export default {
                 this.images[imgIndex].src = URL.createObjectURL(file[0])
                 this.images.push({ index: this.images.length, src: null })
             }
+        },
+        'selectedAsBackground': function (id) {
+            this.background = id; 
         }
     }
 };
