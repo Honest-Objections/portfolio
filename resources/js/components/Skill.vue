c0d08853f54e962c8d7ae0e8e765c93ae22fed09
Chris Clifton <git@chrisclifton.me>
2022-01-21 16:40:34 +0000

    Add missing Skill component

---
+++ resources/js/components/Skill.vue
@@ -0,0 +1,9 @@
+<template>
+    <button class="h-10 px-6 font-semibold rounded-full bg-blue-500 text-white">{{ skill?.name }} ({{ skill?.count+1 }})</button>
+</template>
+
+<script>
+export default {
+    props: ['skill']
+};
+</script>
